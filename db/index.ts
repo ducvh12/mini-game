679edcf03a0127e7e9e69a98403ec528eef298b8
vuduc0612 <ducvu8628@gmail.com>
2026-02-16 08:13:26 +0700

    fix

--- db/index.ts
+++ db/index.ts
@@ -1,10 +1,11 @@
+import { ObjectLiteral } from 'typeorm';
 import { getDataSource } from './data-source';
 import { PlayerSession } from './entities/PlayerSession';
 import { Reward } from './entities/Reward';
 import { SpinLog } from './entities/SpinLog';
 import { EventConfig } from './entities/EventConfig';
 
-export async function getRepository<T>(entity: any) {
+export async function getRepository<T extends ObjectLiteral>(entity: new () => T) {
   const dataSource = await getDataSource();
   return dataSource.getRepository<T>(entity);
 }
